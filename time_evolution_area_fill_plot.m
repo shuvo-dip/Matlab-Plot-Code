bf20450f4f0a278b2ce3f34472cd2ae4e2e019f7
shuvo-dip <44089573+shuvo-dip@users.noreply.github.com>
2022-06-27 18:02:15 +0100

    Update time_evolution_area_fill_plot.m

--- time_evolution_area_fill_plot.m
+++ time_evolution_area_fill_plot.m
@@ -7,15 +7,15 @@
 
 
 clear;close all;clc;
-x = -5:0.001:10;  % Declear an arrey 
-syms x0           % Variable
+x = -5:0.001:10;  % declare an arrey 
+syms x0           % variable
 y = @(x0) 0.8*exp(-0.5*(x-x0).^2); % function, say, Gaussian Distribution
 
-for i = 1:10 % loop for distribtion of different y values
+for i = 1:10 % loop for distribution of different y values
     a0 = sin(pi*i./5);
     b0 = cos(pi*i./5)+5;
-fill(x,y(a0)+0.5*i,'r','FaceAlpha',0.3,'EdgeColor','None') % Importent command, fill the area and tone down the colour by transperent FaceAlpha value
+fill(x,y(a0)+0.5*i,'r','FaceAlpha',0.3,'EdgeColor','None') % Important command, fill the area and tone down the colour by transparent FaceAlpha value
 hold on
-fill(x,y(b0)+0.5*i,'b','FaceAlpha',0.3,'EdgeColor','None') % Importent command, fill the area and tone down the colour by transperent FaceAlpha value
+fill(x,y(b0)+0.5*i,'b','FaceAlpha',0.3,'EdgeColor','None') % Important command, fill the area and tone down the colour by transparent FaceAlpha value
 
 end
